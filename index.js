a88a10d9f95317af0f58477589eab8ffd98ead7c
Ariel <arcomp123@gmail.com>
2019-12-25 20:43:50 -0800

    sgt-m4-final project feature 6

--- index.js
+++ index.js
@@ -132,6 +132,118 @@ module.exports = app.post('/api/grades', async (req, res, next) => {
 });
 
 
+// PATCH and UPDATE
+module.exports = app.patch('/api/grades/:record_pid', async (req, res, next) => {
+
+    const { record_pid } = req.params;
+        console.log(" req.params.pid: ", record_pid);
+
+    const { course, grade, name } = req.body;
+        console.log(" req.body of update: ", req.body);
+
+    let errors = [];
+    
+    let counter = 0;
+
+    try {
+
+        const [checkStudent] = await db.query(`SELECT pid FROM grades WHERE pid = ?`, [record_pid]);
+
+        console.log("checkStudent: ", checkStudent);
+        console.log("checkStudent.length: ", checkStudent.length);
+        
+        if (checkStudent.length === 0) {
+
+            console.log("req.body of Update is Undefined");
+
+            errors.push(`No record found with an ID of: ${record_pid} `);
+
+            res.status(404).send({
+                code: 404,
+                errors: errors,
+                message: "Bad Patch Request"
+                });
+
+            return;
+        }
+
+        if (!course) {
+
+        //  errors.push('No course name received');
+            counter = counter + 1; 
+
+        }
+
+        if (!name) {
+
+        // errors.push('No student name received');
+           counter = counter + 1; 
+        }
+
+        if (!grade) {
+
+        // errors.push('No grade received');  NOT NEEDE FOR THOSE 3
+           counter = counter + 1; 
+        }
+
+        if (counter === 3) {
+
+           errors.push("No valid fields received to update")
+
+        res.status(400).send({
+            code: 400,
+            errors: errors,
+            message: "Bad Patch Request"
+            });
+
+        return;
+        
+        }
+        //} 
+
+        
+        if (grade) {
+            errors = [];
+
+            if (isNaN(grade) || grade < 0 || grade > 100) {
+
+            errors.push( 'A grade should be a number between 0 and 100');
+
+            res.status(422).send({
+            code: 422,
+            errors: errors,
+            message: "Bad Patch Request"
+            });
+
+            return;           
+
+            }
+        }
+
+        
+        const [results] = await db.execute(`UPDATE grades SET course=?, grade=?, name=? WHERE pid=?`, [course, grade, name, record_pid]);
+
+        const [studentToUpdate] = await db.query(`SELECT pid, course, grade, name, updated AS lastUpdated FROM grades WHERE pid = ?`, [record_pid]);
+
+        res.send({
+            messgae: 'Your student update was successful!',
+            record: studentToUpdate            
+        });
+
+    }
+
+    catch (error) {
+        
+        next(error);
+    }
+    
+
+});
+
+
+
+
+
 app.listen(PORT, () => {
     console.log('The server is listening at localhost ' + PORT);
 });
